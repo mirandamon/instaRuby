5ee0c4fe3a8d5654323914bef9d36fe1b9990dd5
mirandamon <nmiranda4@fordham.edu>
2016-06-03 11:26:07 -0400

    added Bootstrap navbar

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'simple_form', '~> 3.1.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'haml'
+gem 'bootstrap-sass', '~> 3.3.6'
 gem "paperclip", "~> 5.0.0.beta1"
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
   def new
     @post = Post.new
@@ -8,7 +9,7 @@ class PostsController < ApplicationController
     @post = Post.create(post_params)
     redirect_to posts_path
   end
-  
+
   private
 
   def post_params
